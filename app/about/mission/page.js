c1bedc35c1bf805abf58595ceec84cd8504e5d3c
JIRafid <jubayerishfaqrafid@gmail.com>
2025-09-22 23:48:46 +0600

    Basic

---
+++ app/about/mission/page.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function mission() {
+  return (
+    <div className='mt-6'>about mission</div>
+  )
+}
---
+++ app/about/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function about() {
+  return (
+     <main className="mt-10">
+         <div>about us page</div>
+    </main>
+  )
+}
